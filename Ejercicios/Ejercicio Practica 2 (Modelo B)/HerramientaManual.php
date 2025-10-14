e4bbf88e909452e1e6400e831a0d0406cde906b2
Aleksandr Filcakov Sapronov <alexfilcakov@gmail.com>
2025-10-14 13:14:31 +0200

    Añadir clase HerramientaManual.php

---
+++ Ejercicios/Ejercicio Practica 2 (Modelo B)/HerramientaManual.php
@@ -0,0 +1,9 @@
+<?php
+class HerramientaManual extends Herramienta {
+    public function calcularPrecio(int $dias, float $precioDia): float {
+        $precioTotalHerramienta = 0;
+
+        return $precioTotalHerramienta;
+    }
+}
+?>
